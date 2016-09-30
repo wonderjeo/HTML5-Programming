1ad63f9c37e8e8c59c45f67a908fe7afd90023c1
wonderjeo <455708864@qq.com>
2016-09-30 23:00:30 +0800

    Add file

---
+++ chapter5/myLoc.js
@@ -0,0 +1,18 @@
+window.onload=getMyLocation;
+
+function getMyLocation(){
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(displayLocation);
+	}
+	else{
+		alert("Oops, no geolocation support.");
+	}
+}
+
+function displayLocation(position){
+	var latitude=position.coords.latitude;
+	var longitude=position.coords.longitude;
+
+	var div=document.getElementById("location");
+	div.innerHTML="You are at Latitude:"+latitude+", Longitude:"+longitude;
+}
