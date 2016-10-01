b21ccff9d663614a6aa9032f7fcabc52fdda30a4
wonderjeo <455708864@qq.com>
2016-10-01 10:45:29 +0800

    Add showMap function

--- chapter5/myLoc.js
+++ chapter5/myLoc.js
@@ -15,4 +15,42 @@ function displayLocation(position){
 
 	var div=document.getElementById("location");
 	div.innerHTML="You are at Latitude:"+latitude+", Longitude:"+longitude;
+	
+	showMap(position.coords);
+}
+var map;
+
+function showMap(coords){
+	var googleLatAndLong = 
+		new google.maps.LatLng(coords.latitude,
+				coords.longitude);
+	var mapOptions={
+		zoom:10,
+		center:googleLatAndLong,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	var mapDiv=document.getElementById("map");
+	map=new google.maps.Map(mapDiv, mapOptions);
+	var title="Your Location";
+	var content="You are here: "+coords.latitude+","+coords.longitude;
+	addMarker(map, googleLatAndLong, title, content);
+}
+
+function addMarker(map, latlong, title, content){
+	var markerOptions={
+		position:latlong,
+		map:map,
+		title:title,
+		clickable:true
+	};
+	var marker=new google.maps.Marker(markerOptions);
+	var infoWindowOptions={
+		content:content,
+		position:latlong
+	};
+	var infoWindow=new google.maps.InfoWindow(infoWindowOptions);
+
+	google.maps.event.addListener(marker,"click", function(){
+		infoWindow.open(map);
+	});
 }
